0a25f9be372043e272bb5784e80a2882518af58b
Niklas Restle <29370294+nckre@users.noreply.github.com>
2023-10-24 16:43:42 +0300

    Removed config module

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
+import { DatabaseModule } from '@app/common';
 import { RetailersModule } from './retailers/retailers.module';
+import { StoresModule } from './stores/stores.module';
 
 @Module({
-  imports: [RetailersModule],
+  imports: [RetailersModule, StoresModule],
   controllers: [ApiController],
   providers: [ApiService],
 })
